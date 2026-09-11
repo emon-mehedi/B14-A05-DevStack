b86228bbbab5dd677cfd6e2f85188135a9b4a7a7
emon-mehedi <emonmehedi02@gmail.com>
2026-09-12 05:18:07 +0600

    Nav made responsive

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,27 @@
 import logo from '../assets/logo-text.png'
+import hamburger from '../assets/hamburger.png'
+import { RxCross1 } from 'react-icons/rx'
+import { useState } from 'react'
 
 export function Nav(){
+  const [sidebarAside, setSidebarAside]=useState<boolean>(true);
+
   return(
     <nav className='shadow sticky top-0 bg-white z-10'>
-      <div className='flex flex-row container mx-auto justify-between items-center py-2'>
+      <aside className={`md:hidden absolute bg-white p-5 pb-10 shadow transition-transform duration-1000 flex flex-row justify-between items-start w-full ${sidebarAside? '-translate-x-full':'translate-x-0'}`}>
+        <ul className='flex flex-col [&>li]:mx-2 [&>li]:my-2'>
+          <li><a href="#" className='text-[#a9256b]'>Home</a></li>
+          <li><a href="#">Technologies</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <button className="p-2" onClick={()=>setSidebarAside(true)}><RxCross1/></button>
+      </aside>
+      <div className='flex flex-row container mx-auto justify-between items-center px-2 py-2'>
+        <img src={hamburger} alt="hamburger" className='md:hidden' onClick={()=>setSidebarAside(false)}/>
         <img src={logo} alt="logo" />
-        <ul className='flex flex-row [&>li]:mx-2'>
+        <ul className='hidden md:flex md:flex-row md:[&>li]:mx-2'>
           <li><a href="#" className='text-[#a9256b]'>Home</a></li>
           <li><a href="#">Technologies</a></li>
           <li><a href="#">Projects</a></li>
--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -37,8 +37,8 @@ export function Technologies({promise}:ITechnologiesProps){
       <Toaster position="top-right"/>
       <h1 className="text-6xl mb-3">Explore the <span className=" text-[#cc4fb8]">Technolgies</span></h1>
       <p className="mb-3">Pick one technology per category to build your ideal stack</p>
-      <div className="flex flex-row gap-6">
-        <div className="grid grid-cols-3 gap-3 flex-3">
+      <div className="flex flex-col md:flex-row md:gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 flex-3">
           {
             techList.map(tech=><Tech key={tech.id} tech={tech} handleStack={handleStack} stack={stack}/>)
           }
