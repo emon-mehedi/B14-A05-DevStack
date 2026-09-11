bc91821f2f0b53c2a94b1d6e5fc9b3b5709dec76
emon-mehedi <emonmehedi02@gmail.com>
2026-09-11 14:17:18 +0600

    Nav bar made sticky to top

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ import logo from '../assets/logo-text.png'
 
 export function Nav(){
   return(
-    <nav className='shadow'>
+    <nav className='shadow sticky top-0 bg-white z-10'>
       <div className='flex flex-row container mx-auto justify-between items-center py-2'>
         <img src={logo} alt="logo" />
         <ul className='flex flex-row [&>li]:mx-2'>
